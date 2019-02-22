d3647fb60985ae4e218101d638b8727c06623878
Mary Liu <mary.y.liu@berkeley.edu>
2019-02-21 18:28:55 -0800

    created footer var

--- app/javascript/packs/beehive2/components/landingPage.jsx
+++ app/javascript/packs/beehive2/components/landingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+const Footer = ({title}) => (<footer>{title}</footer>);
 class LandingPage extends React.Component {
   render() {
     return(
       <div>
         <h1>Hello World. Time for react</h1>
+         <Footer title={footer}/>
       </div>
     )
   }
