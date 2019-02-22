d41417bd59875af5289f051bb84fced3afeb37bf
Mary Liu <mary.y.liu@berkeley.edu>
2019-02-21 19:46:34 -0800

    created footer with labels

--- app/javascript/packs/beehive2/components/landingPage.jsx
+++ app/javascript/packs/beehive2/components/landingPage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-const Footer = ({title}) => (<footer>{title}</footer>);
 class LandingPage extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Hello World. Time for react</h1>
-         <Footer title={footer}/>
+      <div style={{width:'100%'}}>
+        <div>
+          <h1>Hello World. Time for react</h1>
+        </div>
+        <div style={{ position: 'fixed', bottom: '0'}}>
+          <footer style={{display: 'flex', justifyContent: 'space-between', backgroundColor: '#03a9f4'}}>
+            <div>
+              <h2>Logo</h2>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+              <h2>About Us </h2>
+              <h2>About Research </h2>
+              <h2>Contact Us</h2>
+            </div>
+          </footer>
+        </div>
       </div>
     )
   }
+
+
 }
 export default LandingPage
